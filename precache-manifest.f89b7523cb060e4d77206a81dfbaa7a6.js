58117619179affe54947d2501a97ff68cabe6525
Steve Cotter <steve.cotter@kinertia.com>
2018-11-01 08:19:15 -0400

    Updates

---
+++ precache-manifest.f89b7523cb060e4d77206a81dfbaa7a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ea5033fdc4141105e93",
+    "url": "/healthy-ride-pgh/static/js/runtime~main.0ea5033f.js"
+  },
+  {
+    "revision": "a3ca707687a5c4c97dda",
+    "url": "/healthy-ride-pgh/static/js/main.a3ca7076.chunk.js"
+  },
+  {
+    "revision": "d75e2dc44f31b203bb40",
+    "url": "/healthy-ride-pgh/static/js/1.d75e2dc4.chunk.js"
+  },
+  {
+    "revision": "a3ca707687a5c4c97dda",
+    "url": "/healthy-ride-pgh/static/css/main.0c26ac9a.chunk.css"
+  },
+  {
+    "revision": "11b3ad05796cc50c7028d7326427f95e",
+    "url": "/healthy-ride-pgh/index.html"
+  }
+];
